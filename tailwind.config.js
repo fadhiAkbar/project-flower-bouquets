45db32002e26a0a413639543d38ade211269dad5
Fadhil Akbar Djunaedi <fadhilakbar57894@gmail.com>
2025-05-07 10:10:48 +0700

    add git ignore

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: ['Poppins', ''],
+      libre: ['Libre Baskerville', 'serif'],
+    },
+  },
+  plugins: [],
+}
